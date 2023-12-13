d2491b1909de4c5db737ccff626c8ac6d490bcb8
hykim <hiyoung.kimmy@gmail.com>
2023-12-13 10:03:23 -0800

    Style: warehouse desktop

--- src/components/WarehouseList/WarehouseList.jsx
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -3,24 +3,25 @@ import React from 'react'
 // icons 
 import delete_icon from '../../assets/icons/delete_outline-24px.svg';
 import edit from '../../assets/icons/edit-24px.svg';
+import './WarehousList.scss';
 
 const WarehouseList = ({ props }) => {
   const { warehouse_name, address, contact_name, contact_phone, contact_email } = props;
 
   return (
-    <tr>
-    <td>{warehouse_name}</td>
-    <td>{address}</td>
-    <td>{contact_name}</td>
-    <td>
-      <span>{contact_phone}</span>
-      <span>{contact_email}</span>
-    </td>
-    <td>
-      {/* to do : icon  */}
-      <img src={delete_icon} alt="delete icon" />
-      <img src={edit} alt="edit icon" />
-    </td>
+    <tr className="list">
+      <td className="list__name">{warehouse_name} > </td>
+      <td>{address}</td>
+      <td>{contact_name}</td>
+      <td>
+        <span>{contact_phone}</span>
+        <br />
+        <span>{contact_email}</span>
+      </td>
+      <td>
+        <img src={delete_icon} alt="delete icon" />
+        <img src={edit} alt="edit icon" />
+      </td>
   </tr>
   )
 }
