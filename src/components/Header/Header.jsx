f191652e9fa0f44e11db5c019472c4e5e9c20cf3
ReidGibson-Bingham <jsr3b0rn@gmail.com>
2023-12-12 21:23:43 -0800

    add header at all responsive breakpoints.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,18 @@
-import logo from "./../../assets/Logo/inStock-Logo_1x.png";
+import logo from "./../../assets/Logo/inStock-Logo_2x.png";
+// import logoTablet from "./../../assets/Logo/inStock-Logo_2x.png";
 import './Header.scss';
 
 const Header = () => {
     return (
         <header className="header">
             <section className="header__logo-container">
-                <img className="header__logo" src={logo}></img>
+                <img className="header__logo" src={logo} />
+                {/* <img className="header__logo--tablet" src={logoTablet} /> */}
             </section>
 
             <section className="header__pages">
-                <p className="header__pages-warehouse"> warehouses </p>
-                <p className="header__pages-inventory"> inventories </p>
+                <p className="header__pages-warehouse"> Warehouses </p>
+                <p className="header__pages-inventory"> Inventories </p>
             </section>
         </header>
     )
