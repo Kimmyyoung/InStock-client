47f4b8a7218a3e83fa4ace97599cd73503013519
ReidGibson-Bingham <jsr3b0rn@gmail.com>
2023-12-13 10:03:36 -0800

    make necessary styling changes before merging develop.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.scss'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <>
+      <Header/>
       <div>
         <h1>App</h1>
       </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,15 +5,17 @@ import './Header.scss';
 const Header = () => {
     return (
         <header className="header">
-            <section className="header__logo-container">
-                <img className="header__logo" src={logo} />
-                {/* <img className="header__logo--tablet" src={logoTablet} /> */}
-            </section>
+            
+                <section className="header__logo-container">
+                    <img className="header__logo" src={logo} />
+                    {/* <img className="header__logo--tablet" src={logoTablet} /> */}
+                </section>
 
-            <section className="header__pages">
-                <p className="header__pages-warehouse"> Warehouses </p>
-                <p className="header__pages-inventory"> Inventories </p>
-            </section>
+                <section className="header__pages">
+                    <p className="header__pages-name"> Warehouses </p>
+                    <p className="header__pages-name"> Inventories </p>
+                </section>
+            
         </header>
     )
 }
