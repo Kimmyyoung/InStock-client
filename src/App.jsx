ff417c35e80b00e6322d746816a7293b8851b95d
robbie-carragher <orcafinweb@gmail.com>
2023-12-12 20:10:49 -0800

    Routes more complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Warehouses from "./pages/Warehouses/Warehouses";
 import Inventory from "./pages/Inventory/Inventory";
 import AddEdit from "./pages/Add-Edit/Add-Edit";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
 import "./App.scss";
 
@@ -10,8 +11,13 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Warehouses />} />
+        {/* <Route path="/warehouses:warehouseId" element={<Warehouses />} /> */}
         <Route path="/inventory" element={<Inventory />} />
+        {/* <Route path="inventory/:inventoruId" element={<Inventory />} /> */}
         <Route path="add-edit" element={<AddEdit />} />
+        {/* <Route path="/addEdit/:addEditId" element={<AddEdit />} /> */}
+        <Route path="*" element={<NotFoundPage />} />
+
       </Routes>
     </BrowserRouter>
   );
