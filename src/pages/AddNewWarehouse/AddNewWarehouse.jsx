38f3adcdc6c6b3b6aa7d0aa50093083134799bd8
Aisha Atta <aisha.atta1@gmail.com>
2023-12-14 12:02:08 -0800

    Add Hover effects on buttons

--- src/pages/AddNewWarehouse/AddNewWarehouse.jsx
+++ src/pages/AddNewWarehouse/AddNewWarehouse.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react';
-import arrow_back from "../../assets/Icons/arrow_back-24px.svg";
-import { NavLink } from 'react-router-dom';
 import "./AddNewWarehouse.scss";
 import AddWarehouse from '../../components/AddWarehouse/AddWarehouse';
 
